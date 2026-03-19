ce60491d333db0826800e3c0cc411d4d4f02af58
zmstone <zmstone@gmail.com>
2026-03-19 15:22:04 +0100

    build: fix merge conflict

--- lib/public_key/src/public_key.erl
+++ lib/public_key/src/public_key.erl
@@ -723,7 +723,6 @@ get_asn1_module('IssuerAndSerialNumber') -> 'CryptographicMessageSyntax-2009';
 get_asn1_module('OCSPRequest') -> 'OCSP-2024-08';
 get_asn1_module('CertID') -> 'OCSP-2024-08';
 get_asn1_module('Nonce') -> 'OCSP-2024-08';
-get_asn1_module('OCSPRequest') -> 'OCSP-2024-08';
 get_asn1_module('OCSPResponse') -> 'OCSP-2024-08';
 get_asn1_module('BasicOCSPResponse') -> 'OCSP-2024-08';
 get_asn1_module('ResponseData') -> 'OCSP-2024-08';
